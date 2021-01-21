2480e7e5ce9179cf11066a80bcbd89d4f158bb60
yourash <momentum.bots@gmail.com>
2021-01-21 18:46:20 +0200

    Init vuetify

---
+++ client/src/components/MainPage.vue
@@ -0,0 +1,34 @@
+<template>
+  <v-container>
+    <v-row class="text-center">
+
+      <v-row
+      align="center"
+      justify="center"
+    >
+      <v-col
+        class="text-center"
+        cols="12"
+      >
+        <h1 class="display-1 font-weight-thin mb-4">
+          Гречка!
+        </h1>
+        <h4 class="subheading">
+          Найди ее здесь!
+        </h4>
+      </v-col>
+    </v-row>
+
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+  export default {
+    name: 'MainPage',
+
+    data: () => ({
+    
+    }),
+  }
+</script>
